7d738dfc45f97f261916b90b75fdc6f04784cf8a
Henok Kirubel <henokhm2@gmail.com>
2023-07-28 09:11:21 +0300

    Add getCartItems to fetch from API

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
-import { calculateTotals } from './features/cart/cartSlice';
+import { getCartItems, calculateTotals } from './features/cart/cartSlice';
 import Modal from './components/Modal';
 
 function App() {
   const dispatch = useDispatch();
-  const { cartItems } = useSelector((store) => store.cart);
+  const { isLoading, cartItems } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems, dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,8 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit';
-import cartItems from '../../cartItems';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+  return fetch(url)
+    .then((resp) => resp.json())
+    .catch((error) => console.log(error));
+});
+
 
 const initialState = {
-  cartItems,
+  cartItems: [],
   amount: 4,
   total: 0,
   isLoading: true,
@@ -36,6 +44,18 @@ const cartSlice = createSlice({
       });
       state.amount = amount;
       state.total = total.toFixed(2);
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
     },
   },
 });
