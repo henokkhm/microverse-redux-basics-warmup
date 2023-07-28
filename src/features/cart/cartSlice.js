6479ac6be7d42896d562507b8f125724204e9a32
Henok Kirubel <henokhm2@gmail.com>
2023-07-28 09:26:33 +0300

    Use builer notation for lifecycle methods in cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -57,17 +57,20 @@ const cartSlice = createSlice({
       state.total = total.toFixed(2);
     },
   },
-  extraReducers: {
-    [getCartItems.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [getCartItems.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.cartItems = action.payload;
-    },
-    [getCartItems.rejected]: (state) => {
-      state.isLoading = false;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      });
   },
 });
 
