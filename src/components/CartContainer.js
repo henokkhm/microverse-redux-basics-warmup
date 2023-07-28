c5e676626e2dcb572a2037faa43ae9b2fbe05bad
Henok Kirubel <henokhm2@gmail.com>
2023-07-28 08:47:42 +0300

    Add open/close functionality to Modal buttons

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CartItem from './CartItem';
 import { useSelector, useDispatch } from 'react-redux';
-import { clearCart } from '../features/cart/cartSlice';
+import { openModal } from '../features/modal/modalSlice';
 
 const CartContainer = () => {
   const dispatch = useDispatch();
@@ -38,7 +38,7 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+        <button className="btn clear-btn" onClick={() => dispatch(openModal())}>
           clear cart
         </button>
       </footer>
